1632491562e85d34974ee3076215a241a15fc476
jakebostic <jake_bostic@yahoo.com>
2018-12-13 14:00:26 -0500

    Initial Commit

---
+++ PowersTable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PowersTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Squares and Cubes Table");
+            Console.WriteLine();
+
+            Console.Write("Enter an integer: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            string table = "";
+            table += "Number\tSquared\tCubed\n";
+            table += "======\t=======\t=====\n";
+
+            for (int i = 1; i <= num; i++)
+            {
+                int square = i * i;
+                int cube = i * i * i;
+                table += i + "\t" + square + "\t" + cube + "\n";
+            }
+
+            Console.WriteLine(table);
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey(true);
+
+        }
+    }
+}
