9c75e91a074e8ad0efdf75493292c0bc58db51ce
jakebostic <jake_bostic@yahoo.com>
2018-12-13 14:08:21 -0500

    added while loop

--- PowersTable/Program.cs
+++ PowersTable/Program.cs
@@ -9,26 +9,31 @@ namespace PowersTable
             Console.WriteLine("Welcome to the Squares and Cubes Table");
             Console.WriteLine();
 
-            Console.Write("Enter an integer: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-
-            string table = "";
-            table += "Number\tSquared\tCubed\n";
-            table += "======\t=======\t=====\n";
-
-            for (int i = 1; i <= num; i++)
+            string choice = "y";
+            while (choice.Equals("y"))
             {
-                int square = i * i;
-                int cube = i * i * i;
-                table += i + "\t" + square + "\t" + cube + "\n";
+                Console.Write("Enter an integer: ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+
+                string table = "";
+                table += "Number\tSquared\tCubed\n";
+                table += "======\t=======\t=====\n";
+
+                for (int i = 1; i <= num; i++)
+                {
+                    int square = i * i;
+                    int cube = i * i * i;
+                    table += i + "\t" + square + "\t" + cube + "\n";
+                }
+
+                Console.WriteLine(table);
+                Console.WriteLine();
+
+                Console.Write("Continue? (y/n): ");
+                choice = Console.ReadLine();
+                Console.WriteLine();
             }
-
-            Console.WriteLine(table);
-            Console.WriteLine();
-
-            Console.WriteLine("Press any key to exit");
-            Console.ReadKey(true);
 
         }
     }
